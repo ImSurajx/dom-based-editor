947940cdf34be7eddda2d15299f01c2b7b92b4c9
ImSurajx <surajxcode@gmail.com>
2026-01-22 22:26:23 +0530

    feat: implement rectangle creation via drag - use activeToo, use mousedown, mouseup & mousemove combine to create rectangle, - learn about: offsetX & offsetY which calculate mouse position according to current element - implement browser prevents default, so element creation must be perfect
    Signed-off-by: ImSurajx <surajxcode@gmail.com>

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ let rectangleTool = document.getElementById("rectangle-tool");
 let circleTool = document.getElementById("circle-tool");
 let pencilTool = document.getElementById("pencil-tool");
 
+// prevent deafult behaviour
+document.addEventListener('contextmenu', event => event.preventDefault());
+
+
 // selecting workspace
 let workSpace = document.getElementById("work-space");
 
@@ -38,4 +42,55 @@ pencilTool.addEventListener('click', () => {
     activeTool = pencilTool;
     activeTool.classList.add('active-tool');
 });
+
+// creating a dynamic element
+let startX = 0, startY = 0, isCreating = false;
+let currentX = 0, currentY = 0;
+let rectangle = null;
+workSpace.addEventListener('mousedown', (e) => {
+    // decide: we are creating a element or selecting
+    if (activeTool !== selectTool) {
+        isCreating = true;
+    } else {
+        console.log(isCreating);
+        isCreating = false;
+    }
+    startX = e.offsetX; // calculate initial position of element in x plane
+    startY = e.offsetY; // calculate initial position of element in y plane
+    console.log(startX);
+    console.log(startY);
+    console.log(activeTool);
+    // creating a rectangle when tool is active
+    if (isCreating === true) {
+        rectangle = document.createElement('div');
+        rectangle.style.position = "absolute";
+        rectangle.style.left = `${startX}px`;
+        rectangle.style.top = `${startY}px`;
+        rectangle.style.width = "0px";
+        rectangle.style.height = "0px";
+        rectangle.style.border = "1px solid black";
+        workSpace.appendChild(rectangle);
+        console.log("rectangle created");
+    }
+    console.log(rectangle);
+    console.log(activeTool === selectTool);
+});
+// draging create element
+workSpace.addEventListener('mousemove', (e) => {
+    console.log("mousemove");
+    if (isCreating === false || rectangle === null) return;
+    currentX = e.offsetX;
+    currentY = e.offsetY;
+    rectangle.style.width = `${currentX - startX}px`
+    rectangle.style.height = `${currentY - startY}px`
+});
+// stop draging, & element will be created
+workSpace.addEventListener('mouseup', (e)=>{
+    if(isCreating === false) return;
+    else {
+        isCreating = false;
+    }
+    rectangle = null;
+})
+
 
